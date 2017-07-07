3390d2b41a90404b7ed7c7b2cdc02d5113da023f
zengyuetian <zengyuetian@cloutropy.com>
2017-07-07 14:16:38 +0800

    add strings code

---
+++ src/main/java/testing/HamcrestDemo.java
@@ -0,0 +1,32 @@
+package testing;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+// BEGIN main
+public class HamcrestDemo {
+
+    @Test
+    public void testNameConcat() {
+        Person p = new Person("Ian", "Darwin");
+        String f = p.getFullName();
+        assertEquals("name", "Ian Darwin", f);
+    }
+}
+// END main
+
+
+class Person {
+    String first;
+    String last;
+
+    public Person(String s1, String s2 ) {
+        this.first = s1;
+        this.last = s2;
+    }
+
+    public String getFullName() {
+        return (this.first + " " + this.last);
+    }
+
+}
